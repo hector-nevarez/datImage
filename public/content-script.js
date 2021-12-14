8eeebf5e2eafe993834d90e8362c36b1ae4807c4
hector nevarez <hanevarezg@gmail.com>
2021-12-14 13:34:13 -0800

    This renders an image inside the popup view

--- public/content-script.js
+++ public/content-script.js
@@ -4,6 +4,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // imageNodeList.forEach((image) => {
     //     console.log(image.src);
     // })
-    let imageArray= Array.from(imageNodeList);
-    sendResponse({imageArray});
+    let imageArray = Array.from(imageNodeList);
+    imageArray = imageArray.map((image) => {
+        // console.log(image.src);
+        return image.src;
+    })
+    console.log('This is imageArray in content-script:', imageArray)
+    sendResponse({ imageArray });// <-- was doing this
+    // chrome.storage.local.set({ imageArray })
 })
--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import React, { useState, useEffect, useRef } from "react";
 
 
 function App(props) {
-  const [imageArr, setImageArr]= useState(null);
+  const [imageArr, setImageArr] = useState(null);
+  let images;
 
-  useEffect(() => {
-    chrome.storage.local.get('imageArray', function (data) {
-      setImageArr(data);
-    });
-    console.log('This is the array of images inside App.js!',imageArr)
-  }, [])
 
   // const getCurrentTab = async () => {
   //   let queryOptions = { active: true, currentWindow: true };
@@ -18,23 +13,42 @@ function App(props) {
   // return tab;
   // }
 
-  // const handleClick = () => {
-  //   document.chrome.tabs.query({
-  //     currentWindow: true,
-  //     active: true
-  //   },
-  //     function (tabs) {
-  //       document.chrome.tabs.sendMessage(tabs[0].id, 'hi')
-  //     })
-  // }
+  const handleClick = () => {
+    chrome.tabs.query({ currentWindow: true, active: true },
+      function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'hi', setImageArray)
+      })
+    function setImageArray(res) {
+      chrome.storage.local.set({ imageArray: res })
+      // setImageArr(res);
+      // -------The code below renders an image in the popup view -----
+      // const div = document.createElement('img')
+      // div.src= `${res.imageArray[0]}`
+      // document.body.appendChild(div)
+      //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    }
+    // console.log('This is the array of images inside App.js!', imageArr)
+  }
+
   // console.log('This is the array:',props.imageArray);
   return (
     <div className="App">
       <h1>Image Information</h1>
       <div>
-        <button>Scan Images</button>
+        <button onClick={handleClick} >Scan Images</button>
       </div>
-
+      {/* <div>
+        {
+          (!images) ?
+            <p>Click the button to see the details</p> :
+            // imageArr.forEach((image, index)=>{
+            //   <div key={index}>
+            //     <p>{image}</p>
+            //   </div>
+            // })
+            <p>{images}</p>
+        }
+      </div> */}
     </div>
   );
 }
