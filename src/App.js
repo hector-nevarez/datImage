fcb1d5b432a29b73cf4df6636a06634fc6eac05f
hector nevarez <hanevarezg@gmail.com>
2021-12-15 01:34:51 -0800

    BIG Modification. Had to move to a certain version of tensorflow beacause it wasnt working. Change the way image had to be selected because the useRef was giving the wrong image

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-/*global chrome*/
+// /*global chrome*/
 import React, { useState, useEffect, useRef } from "react";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 
-// dummyLinks = [
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Steve_Davis_at_Snooker_German_Masters_%28Martin_Rulsch%29_2014-01-29_06.jpg/145px-Steve_Davis_at_Snooker_German_Masters_%28Martin_Rulsch%29_2014-01-29_06.jpg",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/TCMI_Atari810_front.jpg/161px-TCMI_Atari810_front.jpg",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/2021%2C_08%2C_Zach_Collaros.jpg/118px-2021%2C_08%2C_Zach_Collaros.jpg",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Adolf_Eichmann_at_Trial1961.jpg/121px-Adolf_Eichmann_at_Trial1961.jpg",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Kabukicho_red_gate_and_colorful_neon_street_signs_at_night%2C_Shinjuku%2C_Tokyo%2C_Japan.jpg/350px-Kabukicho_red_gate_and_colorful_neon_street_signs_at_night%2C_Shinjuku%2C_Tokyo%2C_Japan.jpg",
-//   "https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Commons-logo.svg/31px-Commons-logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/MediaWiki-2020-icon.svg/35px-MediaWiki-2020-icon.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Wikimedia_Community_Logo.svg/35px-Wikimedia_Community_Logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wikibooks-logo.svg/35px-Wikibooks-logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Wikidata-logo.svg/47px-Wikidata-logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Wikinews-logo.svg/51px-Wikinews-logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wikiquote-logo.svg/35px-Wikiquote-logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Wikisource-logo.svg/35px-Wikisource-logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Wikispecies-logo.svg/35px-Wikispecies-logo.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Wikiversity_logo_2017.svg/41px-Wikiversity_logo_2017.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Wikivoyage-Logo-v3-icon.svg/35px-Wikivoyage-Logo-v3-icon.svg.png",
-//   "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Wiktionary-logo-v2.svg/35px-Wiktionary-logo-v2.svg.png",
-//   "https://en.wikipedia.org/static/images/footer/wikimedia-button.png",
-//   "https://en.wikipedia.org/static/images/footer/poweredby_mediawiki_88x31.png"
-// ];
+const imageArray = [
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Steve_Davis_at_Snooker_German_Masters_%28Martin_Rulsch%29_2014-01-29_06.jpg/145px-Steve_Davis_at_Snooker_German_Masters_%28Martin_Rulsch%29_2014-01-29_06.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/TCMI_Atari810_front.jpg/161px-TCMI_Atari810_front.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/2021%2C_08%2C_Zach_Collaros.jpg/118px-2021%2C_08%2C_Zach_Collaros.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Adolf_Eichmann_at_Trial1961.jpg/121px-Adolf_Eichmann_at_Trial1961.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Kabukicho_red_gate_and_colorful_neon_street_signs_at_night%2C_Shinjuku%2C_Tokyo%2C_Japan.jpg/350px-Kabukicho_red_gate_and_colorful_neon_street_signs_at_night%2C_Shinjuku%2C_Tokyo%2C_Japan.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Commons-logo.svg/31px-Commons-logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/MediaWiki-2020-icon.svg/35px-MediaWiki-2020-icon.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Wikimedia_Community_Logo.svg/35px-Wikimedia_Community_Logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wikibooks-logo.svg/35px-Wikibooks-logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Wikidata-logo.svg/47px-Wikidata-logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Wikinews-logo.svg/51px-Wikinews-logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wikiquote-logo.svg/35px-Wikiquote-logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Wikisource-logo.svg/35px-Wikisource-logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Wikispecies-logo.svg/35px-Wikispecies-logo.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Wikiversity_logo_2017.svg/41px-Wikiversity_logo_2017.svg.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Wikivoyage-Logo-v3-icon.svg/35px-Wikivoyage-Logo-v3-icon.svg.png",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Wiktionary-logo-v2.svg/35px-Wiktionary-logo-v2.svg.png",
+  "https://en.wikipedia.org/static/images/footer/wikimedia-button.png",
+  "https://en.wikipedia.org/static/images/footer/poweredby_mediawiki_88x31.png"
+];
 
 function App(props) {
-  const { imageArray } = props;
+  // const { imageArray } = props;
   const [isModelLoading, setIsModelLoading] = useState(false);
   const [model, setModel] = useState(null)
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const imageRef = useRef()
 
   const loadModel = async () => {
     setIsModelLoading(true);
     try {
-      const model = await mobilenet.load();
-      setModel(model);
+      const modelLoaded = await mobilenet.load();
+      // console.log('This is the modelLoade from App.js:', modelLoaded);
+      setModel(modelLoaded);
       setIsModelLoading(false);
     } catch (error) {
       console.log(error);
@@ -47,10 +49,15 @@ function App(props) {
     loadModel();
   }, []);
 
-  // if (isModelLoading) {
-  //   return <h2>Model Loading...</h2>
-  // }
-  console.log('This is the images from App.js:', imageArray);
+  // console.log('This is the images from App.js:', imageArray);
+  const handleIdentify = async () => {
+    const predictions = await model.classify(imageRef.current);
+    console.log('This is the Identification results from App.js:', predictions);
+  }
+
+  const handleSelect = (index) => {
+    setSelectedImage(index)
+  }
 
   return (
     <div className="App">
@@ -63,17 +70,17 @@ function App(props) {
               (isModelLoading /* && !imageArray */) ?
                 <h2>Model Loading...</h2> :
                 imageArray.map((image, index) => {
-                  // const div = document.createElement('img')
-                  // div.src = image
-                  // document.body.appendChild(div);
                   return (
                     <div key={index}>
-                      <img src={image} alt="Image obtained from the page" crossOrigin="anonymous" ref={imageRef} />
+                      <img src={image} alt="Image obtained from the page" crossOrigin="anonymous" />
+                      <button className='button' onClick={() => handleSelect(index)}>Select Image</button>
                     </div>
                   )
                 })
             }
           </div>
+          <img src={imageArray[selectedImage]} alt="Image obtained from the page" crossOrigin="anonymous" ref={imageRef} />
+          <button className='button' onClick={handleIdentify}>Identify Image</button>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-/*global chrome*/
+// /*global chrome*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-chrome.storage.local.get(null, function (data) {
-  ReactDOM.render(
-    <App {...data} />,
-    document.getElementById('root')
-  )
-});
+// chrome.storage.local.get(null, function (data) {
+ReactDOM.render(
+  <App /* {...data} */ />,
+  document.getElementById('root')
+)
+// });
