f6c48cf379c0bf3a8f86abf2d94a4aff9763d74b
hector nevarez <hanevarezg@gmail.com>
2021-12-14 20:07:03 -0800

    This is the starting point for building the image detector

--- src/App.js
+++ src/App.js
@@ -4,56 +4,20 @@ import React, { useState, useEffect, useRef } from "react";
 
 function App(props) {
   const [images, setImages] = useState(null);
-  // let images;
 
 
-  // const getCurrentTab = async () => {
-  //   let queryOptions = { active: true, currentWindow: true };
-  // let [tab] = await chrome.tabs.query(queryOptions);
-  // return tab;
-  // }
-
-  // const handleClick = () => {
-  //   chrome.tabs.query({ currentWindow: true, active: true },
-  //     function (tabs) {
-  //       chrome.tabs.sendMessage(tabs[0].id, { scan: 'scanImages' }, setImageArray)
-  //     })
-  //   function setImageArray(res) {
-  //     chrome.storage.local.set({ imageArray: res.imageArray })
-  //     // images = res.imageArray;
-  //     // -------The code below renders an image in the popup view -----
-  //     // const div = document.createElement('img')
-  //     // div.src= `${res.imageArray[0]}`
-  //     // document.body.appendChild(div)
-  //     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //   }
-  //   // console.log('This is the array of images inside App.js!', imageArr)
-  // }
-
-  // const handleClickRetrieve = () => {
-  //   chrome.storage.local.get(['imageArray'], (results) => {
-  //     if (results.imageArray) {
-  //       setImages(results.imageArray);
-  //     }
-  //   });
-  // }
 
   return (
     <div className="App">
       <h1>Image Information</h1>
-      {/* <div>
-        <button onClick={handleClick} >Scan Images</button>
-      </div> */}
-      {/* <div>
-        <button onClick={handleClickRetrieve} >Get Image Data</button>
-      </div> */}
+      
       <div>
         {
           (!props.imageArray) ?
             <p>Click the "Scan" button first, then the "Get Image data" button to see information from the web related to that image</p> :
             props.imageArray.map((image, index) => {
               const div = document.createElement('img')
-              div.src= image
+              div.src = image
               document.body.appendChild(div);
               // <div key={index}><------------Why this doesn't work!!!!!
               //   <img src={image} />
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-/*global chrome*/
+// /*global chrome*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-chrome.storage.local.get(null, function (data) {
+// chrome.storage.local.get(null, function (data) {
   ReactDOM.render(
-    <App {...data} />,
+    <App /* {...data} */ />,
     document.getElementById('root')
   )
-});
+// });
 
