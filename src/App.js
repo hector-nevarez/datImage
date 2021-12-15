5017919389f0b574a5c0953ab8bcb8920c01fea7
hector nevarez <hanevarezg@gmail.com>
2021-12-14 21:25:11 -0800

    Succesfully added tensorflow to the extension, the problem is that it adds the images to the extension each time it mounts!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 /*global chrome*/
 import React, { useState, useEffect, useRef } from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
 
 
 function App(props) {
-  const [images, setImages] = useState(null);
+  const {imageArray}= props;
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null)
 
+  const loadModel = async () => {
+    setIsModelLoading(true);
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+      setIsModelLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsModelLoading(false);
+    }
+  }
 
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  // if (isModelLoading) {
+  //   return <h2>Model Loading...</h2>
+  // }
+  console.log('This is the images from App.js:', imageArray);
 
   return (
     <div className="App">
       <h1>Image Information</h1>
-      
+
       <div>
         {
-          (!props.imageArray) ?
-            <p>Click the "Scan" button first, then the "Get Image data" button to see information from the web related to that image</p> :
-            props.imageArray.map((image, index) => {
+          (isModelLoading && !imageArray) ?
+            <h2>Model Loading...</h2> :
+            imageArray.map((image, index) => {
               const div = document.createElement('img')
               div.src = image
               document.body.appendChild(div);
